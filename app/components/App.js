66f1039903c383617e05e90cac6565a447978a92
Karen Lin <karenalin@berkeley.edu>
2016-11-26 16:55:07 -0800

    Installation work

--- app/components/App.js
+++ app/components/App.js
@@ -2,7 +2,7 @@ var React = require('react');
 
 var helloWorld = React.createClass({
 	render: function() {
-		return <h1>Is this working? Hello World</h1>;
+		return <h1>Is this working? Yes, whoo!</h1>;
 	}
 });
 
