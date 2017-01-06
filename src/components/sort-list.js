d34697d3f004b0a7fc9b9bc7dbde95146f213638
Karen Lin <karenarialin@gmail.com>
2017-01-05 21:32:51 -0800

    Adding original Python

--- src/components/sort-list.js
+++ src/components/sort-list.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 
 export default class SortList extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			mode: this.props.mode,
+			listToSort: this.props.listToSort
+		};
+	}
 
 	render() {
 		return (
 			<div>
 				<h1>Which do you prefer?</h1>
-				<h2>A. {this.props.listToSort[0]}</h2>
+				<h2>A. {this.state.listToSort[0]}</h2>
 				<h1>OR</h1>
-				<h2>B. {this.props.listToSort[1]}</h2>
+				<h2>B. {this.state.listToSort[1]}</h2>
 			</div>
 		);
 	}
